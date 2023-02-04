70126cbdc14452cf77a52e4772a6b5830f674e20
CristiFati <fati_utcluj@yahoo.com>
2023-02-04 21:55:43 +0200

    Utility function

--- common.py
+++ common.py
@@ -6,7 +6,7 @@ import sys
 import time
 
 
-def int_format(limit):
+def int_fmt(limit):
     return "{{:0{:d}d}}".format(math.ceil(math.log10(max(limit, 1))))
 
 
@@ -15,6 +15,13 @@ def ts_str(timestamp=None, human_readable=False):
     if human_readable:
         return "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(*tm)
     return "{:04d}{:02d}{:02d}{:02d}{:02d}{:02d}".format(*tm)
+
+
+def dims_2d(n):
+    if n <= 0:
+        return 0, 0
+    sq = round(math.sqrt(n))
+        return sq, math.ceil(n / sq)
 
 
 if __name__ == "__main__":

584721de7894778aed2a444b5b921c040f8a976a
CristiFati <fati_utcluj@yahoo.com>
2024-05-12 00:38:02 +0300

    feat: simplify function

--- pycfutils/keyboard.py
+++ pycfutils/keyboard.py
@@ -1,37 +1,10 @@
 #!/usr/bin/env python
 
-import functools
 import sys
 import time
-from typing import Any, Callable, Optional
+from typing import Any
 
 __all__ = ("read_key",)
-
-
-def _read_key(
-    interval: float = 0.5,
-    poll_interval: float = 0.1,
-    kp_func: Optional[Callable[[], bool]] = None,
-    rk_func: Optional[Callable[[], int]] = None,
-    start_func: Optional[Callable[[], Any]] = None,
-    end_func: Optional[Callable[[Any], None]] = None,
-):
-    ctx = start_func() if start_func is not None else None
-    try:
-        poll_interval = (
-            interval / 2.0 if poll_interval > interval / 2.0 else poll_interval
-        )
-        time_end = time.time() + interval
-        while True:
-            if kp_func is not None and kp_func():
-                return rk_func() if rk_func is not None else None
-            if time.time() >= time_end:
-                break
-            time.sleep(poll_interval)
-        return None
-    finally:
-        if end_func is not None:
-            end_func(ctx)
 
 
 if sys.platform[:3].lower() == "win":
@@ -45,7 +18,6 @@ if sys.platform[:3].lower() == "win":
 
     _start_func = None
     _end_func = None
-
 
 else:  # Nix
     import select
@@ -68,18 +40,26 @@ else:  # Nix
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, attrs)
 
 
-read_key = functools.partial(
-    _read_key,
-    kp_func=_key_pressed_func,
-    rk_func=_read_key_func,
-    start_func=_start_func,
-    end_func=_end_func,
-)
-
-del _end_func
-del _start_func
-del _read_key_func
-del _key_pressed_func
+def read_key(
+    interval: float = 0.5,
+    poll_interval: float = 0.1,
+):
+    ctx = _start_func() if _start_func is not None else None
+    try:
+        poll_interval = (
+            interval / 2.0 if poll_interval > interval / 2.0 else poll_interval
+        )
+        time_end = time.time() + interval
+        while True:
+            if _key_pressed_func is not None and _key_pressed_func():
+                return _read_key_func() if _read_key_func is not None else None
+            if time.time() >= time_end:
+                break
+            time.sleep(poll_interval)
+        return None
+    finally:
+        if _end_func is not None:
+            _end_func(ctx)
 
 
 if __name__ == "__main__":

5bb1ef89bcdc94c87c65037defeb973ce302ffa7
CristiFati <fati_utcluj@yahoo.com>
2024-05-12 12:46:58 +0300

    fix: PyGObject dummy mock

--- pycfutils/tests/test_gstreamer.py
+++ pycfutils/tests/test_gstreamer.py
@@ -8,8 +8,40 @@ except Exception:
 
 # @TODO - cfati: Dummy
 class GStreamerTestCase(unittest.TestCase):
-    def test_registry_access(self):
-        if RegistryAccess is None:
-            return
-        ra = RegistryAccess()
-        self.assertIsInstance(ra.contents(), dict)
+    if RegistryAccess is None:  # Mock (PyGObject not installed)
+
+        def test_registry_access(self):
+            import sys
+            from types import ModuleType
+
+            class GstDummy:
+                Bin = None
+                Pipeline = None
+
+                @staticmethod
+                def is_initialized():
+                    return False
+
+                @staticmethod
+                def init():
+                    pass
+
+            print("PyGObject not installed. Run dummy test")
+
+            gi = ModuleType("gi")
+            sys.modules["gi"] = gi
+            gi.repository = ModuleType("gi.repository")
+            gi.repository.Gst = GstDummy
+            gi.require_version = lambda ns, ver: None
+            sys.modules["gi.repository"] = gi.repository
+
+            from pycfutils.gstreamer import RegistryAccess
+
+            ra = RegistryAccess()
+            self.assertIsNotNone(ra)
+
+    else:  # Real test (although dummy)
+
+        def test_registry_access(self):
+            ra = RegistryAccess()
+            self.assertIsInstance(ra.contents(), dict)

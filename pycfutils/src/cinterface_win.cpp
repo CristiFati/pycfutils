116967b77db30954391623eba73a11c4ea4411b5
CristiFati <fati_utcluj@yahoo.com>
2024-05-27 17:31:00 +0300

    fix: simplify / improve hook mechanism

--- pycfutils/src/cinterface_win.cpp
+++ pycfutils/src/cinterface_win.cpp
@@ -1,6 +1,5 @@
 #include <unordered_map>
 #include <utility>
-#include <vector>
 
 #include <Windows.h>
 
@@ -12,7 +11,7 @@
 
 typedef std::unordered_map<DWORD, HHOOK> HookData;
 typedef std::pair<int, int> XY;
-typedef std::unordered_map<DWORD, std::vector<XY>> XYData;
+typedef std::unordered_map<DWORD, XY> XYData;
 
 CRITICAL_SECTION criticalSection;
 XYData xyData;
@@ -28,22 +27,26 @@ static LRESULT CALLBACK _hookFunc(int nCode, WPARAM wParam, LPARAM lParam)
         case HCBT_CREATEWND: {
             EnterCriticalSection(&criticalSection);
             DWORD tid = GetCurrentThreadId();
-            if (hookData.find(tid) != hookData.cend()) {
-                auto xyIt = xyData[tid].cbegin();
-                if (xyIt != xyData[tid].cend()) {
+            HookData::const_iterator hIt = hookData.find(tid);
+            if (hIt != hookData.cend()) {
+                XYData::const_iterator xyIt = xyData.find(tid);
+                if (xyIt != xyData.cend()) {
                     LPCBT_CREATEWNDW pcw = (LPCBT_CREATEWNDW)lParam;
                     if (pcw) {
                         LPCREATESTRUCTW pcs = pcw->lpcs;
                         if (pcs) {
-                            if (xyIt->first != SENTINEL) {
-                                pcs->x = xyIt->first;
+                            if (xyIt->second.first != SENTINEL) {
+                                pcs->x = xyIt->second.first;
                             }
-                            if (xyIt->second != SENTINEL) {
-                                pcs->y = xyIt->second;
+                            if (xyIt->second.second != SENTINEL) {
+                                pcs->y = xyIt->second.second;
                             }
                         }
                     }
-                    xyData[tid].erase(xyIt);
+                    xyData.erase(xyIt);
+                }
+                if (UnhookWindowsHookEx(hIt->second)) {  // @TODO - cfati: Unhook from within hook
+                    hookData.erase(hIt);
                 }
             }
             LeaveCriticalSection(&criticalSection);
@@ -60,15 +63,23 @@ int MessageBoxXY(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType, int x
 {
     EnterCriticalSection(&criticalSection);
     DWORD tid = GetCurrentThreadId();
+    int err = 0;
     if (hookData.find(tid) == hookData.cend()) {
         HHOOK hook = SetWindowsHookExW(WH_CBT, _hookFunc, 0, tid);
         if (hook) {
-            hookData[tid] = hook;
-        }  // @TODO - cfati: Silently fail?
+            hookData.emplace(std::make_pair(tid, hook));
+        }
+        else {
+            err = GetLastError();
+        }
+    } else {
+        err = -1;
     }
-    xyData[tid].emplace_back(std::make_pair(x, y));
+    if (!err) {
+        xyData.emplace(std::make_pair(tid, std::make_pair(x, y)));
+    }
     LeaveCriticalSection(&criticalSection);
-    return MessageBoxW(hWnd, lpText, lpCaption, uType);
+    return err ? err : MessageBoxW(hWnd, lpText, lpCaption, uType);
 }
 
 

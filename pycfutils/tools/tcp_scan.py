e7895b85b22cd7fb7b96d59af19960b15bf7548c
CristiFati <fati_utcluj@yahoo.com>
2024-05-31 18:17:03 +0300

    feat: improve argument handling so that discrete values are also supported besides ranges for IPs, ports

--- pycfutils/tools/tcp_scan.py
+++ pycfutils/tools/tcp_scan.py
@@ -7,6 +7,7 @@ import time
 
 from pycfutils.exceptions import NetworkException
 from pycfutils.io import read_key
+from pycfutils.miscellaneous import uniques
 from pycfutils.network import SOCKET_TYPE_TCP, connect_to_server, parse_address
 
 
@@ -22,21 +23,41 @@ def parse_args(*argv):
         epilog="Scans the IP:port combination in the given ranges",
     )
     parser.add_argument(
-        "--first_ip", "-f", default="127.0.0.1", help="first IP to scan"
+        "--first_address",
+        "-a",
+        action="append",
+        dest="first_addresses",
+        default=[],
+        help="first address to scan. Can be specified multiple times"
+             " (in that case only the specified addresses (resolved IPs) will be attempted)",
     )
-    parser.add_argument("--last_ip", "-l", help="last IP to scan (defaults to first)")
     parser.add_argument(
-        "--first_port", "-p", default=0, type=int, help="first port to scan"
+        "--first_port",
+        "-p",
+        action="append",
+        default=[],
+        dest="first_ports",
+        type=int,
+        help="first port to scan. Can be specified multiple times"
+             " (in that case only the specified ports will be attempted)",
+    )
+    parser.add_argument(
+        "--last_ip",
+        "-i",
+        help="last IP to scan (defaults to first). Must be the same family with first."
+             " If more than one first address is specified (or it resolves to multiple IPs),"
+             " this is ignored",
     )
     parser.add_argument(
         "--last_port",
         "-r",
         default=0,
         type=int,
-        help="last port to scan (defaults to first)",
+        help="last port to scan (defaults to first, must be greater or equal to first)."
+             " If more than one first port is specified, this is ignored",
     )
     parser.add_argument(
-        "--timeout", "-t", default=1, type=float, help="connection timeout"
+        "--timeout", "-t", default=0.5, type=float, help="connection timeout"
     )
 
     args, unk = parser.parse_known_args()
@@ -45,53 +66,99 @@ def parse_args(*argv):
 
     if args.timeout < 0:
         parser.exit(status=-1, message="Timeout can't be negative\n")
-    if args.first_port <= 0 or args.first_port > 0xFFFF:
-        parser.exit(status=-1, message="Invalid first port\n")
-    if args.last_port < 0 or args.last_port > 0xFFFF:
-        parser.exit(status=-1, message="Invalid last port\n")
-    elif args.last_port == 0:
-        args.last_port = args.first_port
-    try:
-        record = parse_address(
-            args.first_ip or "", 0, type_=SOCKET_TYPE_TCP, exact_matches=1
-        )[0]
-    except NetworkException as e:
-        parser.exit(status=-1, message=f"Invalid first IP: {e}\n")
-    ipn = int.from_bytes(socket.inet_pton(record[2], record[0]), byteorder="big")
-    args.first_ip = ipn, record[2]
-    if args.last_ip:
-        try:
-            record = parse_address(
-                args.last_ip or "", 0, type_=SOCKET_TYPE_TCP, exact_matches=1
-            )[0]
-        except NetworkException as e:
-            parser.exit(status=-1, message=f"Invalid last IP: {e}\n")
-        ipn = int.from_bytes(socket.inet_pton(record[2], record[0]), byteorder="big")
-        args.last_ip = ipn, record[2]
+    args.first_ports = uniques(args.first_ports)
+    if not args.first_ports:
+        parser.exit(status=-1, message="At least one first port must be specified\n")
+    for fp in args.first_ports:
+        if fp <= 0 or fp > 0xFFFF:
+            parser.exit(status=-1, message="Invalid first port\n")
+    if len(args.first_ports) == 1:
+        if args.last_port < 0 or args.last_port > 0xFFFF:
+            parser.exit(status=-1, message="Invalid last port\n")
+        elif args.last_port == 0:
+            args.last_port = args.first_ports[0]
     else:
-        args.last_ip = args.first_ip
-    if args.first_ip[-1] != args.last_ip[-1]:
-        parser.exit(status=-1, message="IP families don't match\n")
+        if args.last_port != 0:
+            print("Last port passed with multiple first ports. Ignoring")
+    addr_strs = tuple(e for e in uniques(args.first_addresses) if e)
+    if not addr_strs:
+        parser.exit(
+            status=-1, message=f"At least one first address must be specified\n"
+        )
+    addrs = []
+    for addr_str in addr_strs:
+        try:
+            records = parse_address(addr_str, 0, type_=SOCKET_TYPE_TCP)
+        except NetworkException as e:
+            print(f"Address {addr_str} could not be resolved ({e}). Ignoring")
+            continue
+        for record in records:
+            ipn = int.from_bytes(
+                socket.inet_pton(record[2], record[0]), byteorder="big"
+            )
+            addrs.append((ipn, record[2]))
+    if not addrs:
+        parser.exit(status=-1, message="No valid (first) address passed\n")
+    args.first_addresses = addrs
+    if len(args.first_addresses) == 1:
+        if args.last_ip:
+            try:
+                record = parse_address(
+                    args.last_ip or "", 0, type_=SOCKET_TYPE_TCP, exact_matches=1
+                )[0]
+            except NetworkException as e:
+                parser.exit(status=-1, message=f"Invalid last IP: {e}\n")
+            ipn = int.from_bytes(
+                socket.inet_pton(record[2], record[0]), byteorder="big"
+            )
+            args.last_ip = ipn, record[2]
+        else:
+            args.last_ip = args.first_addresses[0]
+        if args.first_addresses[0][-1] != args.last_ip[-1]:
+            parser.exit(status=-1, message="IP families don't match\n")
+    else:
+        if args.last_ip:
+            print("Last ip passed with multiple first addresses. Ignoring")
 
     return args, unk
 
 
 def main(*argv):
     args, _ = parse_args()
+    ips = len(args.first_addresses) > 1
+    ports = len(args.first_ports) > 1
+    addr_text = (
+        f"addresses: {', ' .join(_ip_string(*e) for e in args.first_addresses)}"
+        if ips
+        else f"from {_ip_string(*args.first_addresses[0])} to {_ip_string(*args.last_ip)}"
+    )
+    port_text = (
+        f"{', ' .join(str(e) for e in args.first_ports)}"
+        if ports
+        else f"from {args.first_ports[0]} to {args.last_port}"
+    )
     print(
-        f"Scanning network from {_ip_string(*args.first_ip)}"
-        f" to {_ip_string(*args.last_ip)}"
-        f" on ports {args.first_port} to {args.last_port}"
+        f"Scanning network ({addr_text}) on ports {port_text}"
         f" with a {args.timeout:.2f}s connection timeout.\n"
         "Press any to interrupt...\n"
     )
-    fam = args.first_ip[-1]
     total, ok = 0, 0
     start_time = time.time()
     brk = False
-    for ip in range(args.first_ip[0], args.last_ip[0] + 1):
-        addr = _ip_string(ip, fam)
-        for port in range(args.first_port, args.last_port + 1):
+    for ip in (
+        args.first_addresses
+        if ips
+        else (
+            (e, args.first_addresses[0][-1])
+            for e in range(args.first_addresses[0][0], args.last_ip[0] + 1)
+        )
+    ):
+        addr = _ip_string(*ip)
+        for port in (
+            args.first_ports
+            if ports
+            else range(args.first_ports[0], args.last_port + 1)
+        ):
             print(
                 "Probing {:s}:{:d} (for {:.2f} seconds)...".format(
                     addr, port, args.timeout
@@ -99,8 +166,11 @@ def main(*argv):
             )
             try:
                 total += 1
+                cur_start_time = time.time()
                 connect_to_server(addr, port, attempts=1, attempt_timeout=args.timeout)
-                print("  --- !!! SUCCESS !!! ---")
+                print(
+                    f"  --- !!! SUCCESS !!! --- (took {time.time() - cur_start_time:.3f} seconds)"
+                )
             except NetworkException:
                 pass
             else:

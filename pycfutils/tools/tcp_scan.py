06635cd841bec247554ee4b7bd9c8009ab3099ad
CristiFati <fati_utcluj@yahoo.com>
2024-06-01 10:07:33 +0300

    fix: fixes and improvements

--- pycfutils/tools/tcp_scan.py
+++ pycfutils/tools/tcp_scan.py
@@ -19,42 +19,50 @@ def _ip_string(ip: int, family: socket.AddressFamily) -> str:
 
 def parse_args(*argv):
     parser = argparse.ArgumentParser(
-        description="IP(v4):Port scanner",
-        epilog="Scans the IP:port combination in the given ranges",
+        description="IP:Port scanner (TCP)",
+        epilog="Scans IP:port combinations in the given specified values or ranges",
     )
     parser.add_argument(
-        "--first_address",
+        "--address",
         "-a",
         action="append",
-        dest="first_addresses",
+        dest="addresses",
         default=[],
-        help="first address to scan. Can be specified multiple times"
-             " (in that case only the specified addresses (resolved IPs) will be attempted)",
+        help=(
+            "address (first) to scan. Can be specified multiple times"
+            " (in that case only the specified addresses (resolved IPs) will be attempted)"
+        ),
     )
     parser.add_argument(
-        "--first_port",
+        "--port",
         "-p",
         action="append",
         default=[],
-        dest="first_ports",
+        dest="ports",
         type=int,
-        help="first port to scan. Can be specified multiple times"
-             " (in that case only the specified ports will be attempted)",
+        help=(
+            "port (first) to scan. Can be specified multiple times"
+            " (in that case only the specified ports will be attempted)"
+        ),
     )
     parser.add_argument(
         "--last_ip",
         "-i",
-        help="last IP to scan (defaults to first). Must be the same family with first."
-             " If more than one first address is specified (or it resolves to multiple IPs),"
-             " this is ignored",
+        help=(
+            "last IP to scan (defaults to first). Must be the same family with first."
+            " If more than one first address is specified (or it resolves to multiple IPs),"
+            " this is ignored"
+        ),
     )
     parser.add_argument(
         "--last_port",
         "-r",
         default=0,
         type=int,
-        help="last port to scan (defaults to first, must be greater or equal to first)."
-             " If more than one first port is specified, this is ignored",
+        help=(
+            "last port to scan (defaults to first, must be greater or equal to first)."
+            " If more than one first port is specified, this is ignored"
+        ),
     )
     parser.add_argument(
         "--timeout", "-t", default=0.5, type=float, help="connection timeout"
@@ -66,27 +74,22 @@ def parse_args(*argv):
 
     if args.timeout < 0:
         parser.exit(status=-1, message="Timeout can't be negative\n")
-    args.first_ports = uniques(args.first_ports)
-    if not args.first_ports:
+    args.ports = tuple(uniques(args.ports))
+    if not args.ports:
         parser.exit(status=-1, message="At least one first port must be specified\n")
-    for fp in args.first_ports:
+    for fp in args.ports:
         if fp <= 0 or fp > 0xFFFF:
             parser.exit(status=-1, message="Invalid first port\n")
-    if len(args.first_ports) == 1:
+    if len(args.ports) == 1:
         if args.last_port < 0 or args.last_port > 0xFFFF:
             parser.exit(status=-1, message="Invalid last port\n")
         elif args.last_port == 0:
-            args.last_port = args.first_ports[0]
+            args.last_port = args.ports[0]
     else:
         if args.last_port != 0:
             print("Last port passed with multiple first ports. Ignoring")
-    addr_strs = tuple(e for e in uniques(args.first_addresses) if e)
-    if not addr_strs:
-        parser.exit(
-            status=-1, message=f"At least one first address must be specified\n"
-        )
     addrs = []
-    for addr_str in addr_strs:
+    for addr_str in args.addresses:
         try:
             records = parse_address(addr_str, 0, type_=SOCKET_TYPE_TCP)
         except NetworkException as e:
@@ -96,11 +99,13 @@ def parse_args(*argv):
             ipn = int.from_bytes(
                 socket.inet_pton(record[2], record[0]), byteorder="big"
             )
-            addrs.append((ipn, record[2]))
+            ip = (ipn, record[2])
+            if ip not in addrs:
+                addrs.append(ip)
     if not addrs:
         parser.exit(status=-1, message="No valid (first) address passed\n")
-    args.first_addresses = addrs
-    if len(args.first_addresses) == 1:
+    args.addresses = addrs
+    if len(args.addresses) == 1:
         if args.last_ip:
             try:
                 record = parse_address(
@@ -113,8 +118,8 @@ def parse_args(*argv):
             )
             args.last_ip = ipn, record[2]
         else:
-            args.last_ip = args.first_addresses[0]
-        if args.first_addresses[0][-1] != args.last_ip[-1]:
+            args.last_ip = args.addresses[0]
+        if args.addresses[0][-1] != args.last_ip[-1]:
             parser.exit(status=-1, message="IP families don't match\n")
     else:
         if args.last_ip:
@@ -125,20 +130,20 @@ def parse_args(*argv):
 
 def main(*argv):
     args, _ = parse_args()
-    ips = len(args.first_addresses) > 1
-    ports = len(args.first_ports) > 1
+    ips = len(args.addresses) > 1
+    ports = len(args.ports) > 1
     addr_text = (
-        f"addresses: {', ' .join(_ip_string(*e) for e in args.first_addresses)}"
+        f"{', ' .join(_ip_string(*e) for e in args.addresses)}"
         if ips
-        else f"from {_ip_string(*args.first_addresses[0])} to {_ip_string(*args.last_ip)}"
+        else f"from {_ip_string(*args.addresses[0])} to {_ip_string(*args.last_ip)}"
     )
     port_text = (
-        f"{', ' .join(str(e) for e in args.first_ports)}"
+        f"{', ' .join(str(e) for e in args.ports)}"
         if ports
-        else f"from {args.first_ports[0]} to {args.last_port}"
+        else f"from {args.ports[0]} to {args.last_port}"
     )
     print(
-        f"Scanning network ({addr_text}) on ports {port_text}"
+        f"Scanning network addresses {addr_text} on ports {port_text}"
         f" with a {args.timeout:.2f}s connection timeout.\n"
         "Press any to interrupt...\n"
     )
@@ -146,19 +151,15 @@ def main(*argv):
     start_time = time.time()
     brk = False
     for ip in (
-        args.first_addresses
+        args.addresses
         if ips
         else (
-            (e, args.first_addresses[0][-1])
-            for e in range(args.first_addresses[0][0], args.last_ip[0] + 1)
+            (e, args.addresses[0][-1])
+            for e in range(args.addresses[0][0], args.last_ip[0] + 1)
         )
     ):
         addr = _ip_string(*ip)
-        for port in (
-            args.first_ports
-            if ports
-            else range(args.first_ports[0], args.last_port + 1)
-        ):
+        for port in args.ports if ports else range(args.ports[0], args.last_port + 1):
             print(
                 "Probing {:s}:{:d} (for {:.2f} seconds)...".format(
                     addr, port, args.timeout

b11baf1b00ed41926e08c8ece0733b2957dfcd30
CristiFati <fati_utcluj@yahoo.com>
2025-05-02 13:09:56 +0300

    fix: minor - command comment

--- _utils/win.bat
+++ _utils/win.bat
@@ -11,8 +11,8 @@ if defined NO_TEST (
     set PYTHONPATH=
     :: @TODO - cfati: End with \ (if no pattern given)
     if not defined TEST_VENV_PATTERN (
-        :: set TEST_VENV_PATTERN="e:\Work\Dev\VEnvs\"
-        set TEST_VENV_PATTERN="e:\Work\Dev\VEnvs\*test0"
+        :: set TEST_VENV_PATTERN="e:\Work\Dev\VEnvs\test0\"
+        set TEST_VENV_PATTERN="e:\Work\Dev\VEnvs\test0\*test0"
     )
     if not defined TEST_WHEEL_DIR (
         set TEST_WHEEL_DIR="e:\Work\Dev\Repos\GitHub\CristiFati\pycfutils\src\dist"
@@ -20,7 +20,7 @@ if defined NO_TEST (
     call bat_funcs dirname %TEST_VENV_PATTERN% _VENVS_DIR
     set _VENVS_DIR=%_VENVS_DIR:"=%
     set _PY_CODE="import sys, pycfutils.gui as pg, pycfutils.gui.effects as pge, pycfutils.io as pio, pycfutils.setup.command.build_clibdll as pscbcd;print('Press a key: ', pio.read_key(timeout=1));print(pg.message_box('MBox', sys.version, 320, 200))"
-    for /f %%g in ('dir /b /ad /a-l %TEST_VENV_PATTERN:"=%') do (
+    for /f %%g in ('dir /b /ad %TEST_VENV_PATTERN:"=%') do (
         echo Using environment: "%_VENVS_DIR%\%%g"
         call "%_VENVS_DIR%\%%g\Scripts\activate.bat"
         python -VV
